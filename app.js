5187dbae4ebe916ecd663bd8417fb0fa636d39be
sobbey99 <sobbey99@gmail.com>
2022-12-28 10:12:38 +0200

    no adding score after gameOver

--- app.js
+++ app.js
@@ -162,6 +162,12 @@ window.addEventListener('load', function(){
             for (let i = 0; i < this.game.ammo; i++) {
                 context.fillRect(20 + 5 * i, 50, 3, 20);
             }
+
+            //timer
+            const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
+            context.fillText('Timer ' + formattedTime, 20, 100);
+
+            //game over messages
             if(this.game.gameOver) {
                 context.textAlign = 'center';
                 let message1;
@@ -200,9 +206,17 @@ window.addEventListener('load', function(){
             this.gameOver = false;
             this.score = 0;
             this.winningScore = 10;
+            this.gameTime = 0;
+            this.timeLimit = 5000;
         }
 
         update(deltaTime){
+            if (!this.gameOver) {
+                this.gameTime += deltaTime;
+            }
+            if(this.gameTime > this.timeLimit) {
+                this.gameOver = true;
+            }
             this.player.update();
             if (this.ammoTimer > this.ammoInterval) {
                 if (this.ammo < this.maxAmmo) {
@@ -224,7 +238,9 @@ window.addEventListener('load', function(){
                         projectile.markedForDeletion = true;
                         if (enemy.lives <= 0) {
                             enemy.markedForDeletion = true;
-                            this.score += enemy.score;
+                            if(!this.gameOver) {
+                                this.score += enemy.score;
+                            }
                             if(this.score > this.winningScore) {
                                 this.gameOver = true ;
                             }
